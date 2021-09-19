91c7529e2f0d00ef4e6bba7ecaa679d9b76b7084
extranuts <QroWka8462>
2021-09-19 16:04:53 +0300

    Refactoring to Factory

--- src/main/java/factory/FactoryAppRun.java
+++ src/main/java/factory/FactoryAppRun.java
@@ -4,30 +4,48 @@ import java.util.Date;
 
 public class FactoryAppRun {
     public static void main(String[] args) {
-        Watch watch = new DigitalWatch();
-        watch.showTime();
 
-        Watch watch1 = new RomeWatch();
-        watch1.showTime();
+//        Watch watch = new DigitalWatch();
+//        watch.showTime();
+//
+//        Watch watch1 = new RomeWatch();
+//        watch1.showTime();
+
+        WatchMaker maker = new DigitalWatchMaker();
+        var watch = maker.createWatch();
+        watch.showTime();
 
     }
 }
+
+interface WatchMaker {
+    Watch createWatch();
+}
+
+class DigitalWatchMaker implements WatchMaker {
+    public Watch createWatch() {
+        return new DigitalWatch();
+    }
+}
+
+class RomainWatchMaker implements WatchMaker {
+    public Watch createWatch() {
+        return new RomeWatch();
+    }
+}
+
 
 interface Watch {
     void showTime();
 }
 
 class DigitalWatch implements Watch {
-
-    @Override
     public void showTime() {
         System.out.println(new Date());
     }
 }
 
 class RomeWatch implements Watch {
-
-    @Override
     public void showTime() {
         System.out.println("VII-XX");
     }
