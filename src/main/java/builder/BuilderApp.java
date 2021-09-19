c1c63f83e87130d2eec80c1a44655b98a02dedb6
extranuts <QroWka8462>
2021-09-19 17:16:05 +0300

    Release Builder

--- src/main/java/builder/BuilderApp.java
+++ src/main/java/builder/BuilderApp.java
@@ -17,7 +17,45 @@ class Car {
     Transmission transmission;
     int maxSpeed;
 
-    public void setMake(String make){this.make = make;}
-    public void setTransmission(Transmission transmission) {this.transmission = transmission;}
-    public void setMaxSpeed(int maxSpeed) {this.maxSpeed = maxSpeed;}
+    public void setMake(String make) {
+        this.make = make;
+    }
+
+    public void setTransmission(Transmission transmission) {
+        this.transmission = transmission;
+    }
+
+    public void setMaxSpeed(int maxSpeed) {
+        this.maxSpeed = maxSpeed;
+    }
+}
+
+class CarBuilder {
+    String m = "Default";
+    Transmission t = Transmission.MANUAL;
+    int s = 120;
+
+    CarBuilder buildMake(String m) {
+        this.m = m;
+        return this;
+    }
+
+    CarBuilder buildTransmission(Transmission t) {
+        this.t = t;
+        return this;
+    }
+
+    CarBuilder buildMaxSpeed(int s) {
+        this.s = s;
+        return this;
+    }
+
+    Car build() {
+        Car car = new Car();
+        car.setMake(m);
+        car.setTransmission(t);
+        car.setMaxSpeed(s);
+        return car;
+    }
+
 }
