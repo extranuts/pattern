9a8cffa2a4c556f863b8f7904e042eb9361665f7
extranuts <QroWka8462>
2021-09-20 15:21:53 +0300

    Mediator pattern

--- src/main/java/behavioral/mediator/MediatorRunner.java
+++ src/main/java/behavioral/mediator/MediatorRunner.java
@@ -1,5 +1,89 @@
 package behavioral.mediator;
 
-public class MediatorRunner {
+import java.util.ArrayList;
+import java.util.List;
 
+public class MediatorRunner {
+    public static void main(String[] args) {
+        TextChat chat = new TextChat();
+
+        User admin = new Admin(chat);
+        User u1 = new SimpleUser(chat);
+        User u2 = new SimpleUser(chat);
+
+        chat.setAdmin(admin);
+        chat.addUser(u1);
+        chat.addUser(u2);
+
+        u1.sendMessage("Hi i'm simple user....");
+        admin.sendMessage("Admin joined to chat.....");
+
+    }
+}
+
+interface User {
+    void sendMessage(String message);
+
+    void getMessage(String message);
+}
+
+class Admin implements User {
+    Chat chat;
+
+    public Admin(Chat chat) {
+        this.chat = chat;
+    }
+
+    @Override
+    public void sendMessage(String message) {
+        chat.sendMessage(message, this);
+    }
+
+    @Override
+    public void getMessage(String message) {
+        System.out.println("Администратор получает сообщение ' " + message + "'");
+    }
+}
+
+class SimpleUser implements User {
+    Chat chat;
+
+    public SimpleUser(Chat chat) {
+        this.chat = chat;
+    }
+
+    @Override
+    public void sendMessage(String message) {
+        chat.sendMessage(message, this);
+    }
+
+    @Override
+    public void getMessage(String message) {
+        System.out.println("Пользователь получает сообщение ' " + message + "'");
+    }
+}
+
+interface Chat {
+    void sendMessage(String message, User user);
+}
+
+class TextChat implements Chat {
+    User admin;
+    List<User> users = new ArrayList<>();
+
+    public void setAdmin(User admin) {
+        this.admin = admin;
+    }
+
+    public void addUser(User user) {
+        users.add(user);
+    }
+
+    @Override
+    public void sendMessage(String message, User user) {
+        for (User u : users) {
+            u.getMessage(message);
+        }
+        admin.getMessage(message);
+    }
 }
