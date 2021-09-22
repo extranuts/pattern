c64a4352e32db98dd9ff961d314fb2037b80a922
extranuts <QroWka8462>
2021-09-22 16:49:45 +0300

    State pattern

---
+++ src/main/java/behavioral/state/StateRunner.java
@@ -0,0 +1,57 @@
+package behavioral.state;
+
+public class StateRunner {
+    public static void main(String[] args) {
+
+        Station dfm = new RadioDFM();
+        Radio radio = new Radio();
+        radio.setStation(dfm);
+
+        for (int i = 0; i < 10; i++) {
+            radio.play();
+            radio.nextStation();
+
+        }
+    }
+}
+
+//state
+interface Station {
+    void play();
+}
+
+class RadioDFM implements Station {
+    @Override
+    public void play() {
+        System.out.println("Play radio DFM . ... .. .");
+    }
+}
+
+class RadioEnergy implements Station {
+    @Override
+    public void play() {
+        System.out.println("Play radio Energy . .. . .. .");
+    }
+}
+
+//Context
+class Radio {
+
+    Station station;
+    void setStation(Station st) {
+        station = st;
+    }
+
+    void nextStation() {
+        if(station instanceof RadioDFM) {
+            setStation(new RadioDFM());
+        }
+        else if (station instanceof RadioEnergy) {
+            setStation(new RadioEnergy());
+        }
+    }
+
+    void play() {
+        station.play();
+    }
+}
