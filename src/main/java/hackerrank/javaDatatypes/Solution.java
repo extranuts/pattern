aa57eaa8252c4efbd96db149484d858577fb8a4a
extranuts <valeev.timur.v@gmail.com>
2022-04-02 22:52:22 +0300

    HackerRank DataTypes Solution

--- src/main/java/hackerrank/javaDatatypes/Solution.java
+++ src/main/java/hackerrank/javaDatatypes/Solution.java
@@ -17,8 +17,10 @@ class Solution {
                 System.out.println(x + " can be fitted in:");
                 if (x >= -128 && x <= 127) System.out.println("* byte");
                 if (x >= -32768 && x <= 32767) System.out.println("* short");
-                if (x >= -2147483648 && x <= 2147483647) System.out.println("* int");
-                if (x >= (-(Math.pow(2, 63))) && x <= (Math.pow(2, 63) - 1)) System.out.println("* long");
+                double int_cutoff = Math.pow(2, 31);
+                if (x >= -Math.pow(2, 31) && x <= Math.pow(2, 31) - 1) System.out.println("* int");
+                double long_cutoff = Math.pow(2, 63);
+                if (x >= -Math.pow(2, 63) && x <= Math.pow(2, 63) - 1) System.out.println("* long");
             } catch (Exception e) {
                 System.out.println(sc.next() + " can't be fitted anywhere.");
             }
